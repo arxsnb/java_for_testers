44152c32df1c876f4daf6be7c66b82ce2c78e76b
arxsnb <arxsnb@mail.ru>
2026-05-15 23:41:13 +0300

    ДЗ 9

--- addressbook_web_tests/src/test/java/tests/ContactCreationTest.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTest.java
@@ -3,8 +3,30 @@ package tests;
 import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactCreationTest extends TestBase {
+
+    public static List<ContactData> contactProvider() {
+        var result = new ArrayList<ContactData>();
+        for (var firstname : List.of("", "firstname")) {
+            for (var middlename : List.of("", "middlename")) {
+                for (var lastname : List.of("", "lastname")) {
+                    result.add(new ContactData().withNames(firstname, middlename, lastname));
+                }
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            result.add(new ContactData().withNames(randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+        }
+
+        return result;
+    }
 
     @Test
     public void CanCreateContact() {
@@ -25,6 +47,16 @@ public class ContactCreationTest extends TestBase {
                 "First name Test " + System.currentTimeMillis() % 10000, "Middle name Test",
                 "Last name Test"
         ));
+        int newContactCount = app.contacts().getCount();
+        Assertions.assertEquals(contactCount + 1, newContactCount);
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("contactProvider")
+    public void CanCreateMultipleContact(ContactData contact) {
+        int contactCount = app.contacts().getCount();
+        app.contacts().createContact(contact);
         int newContactCount = app.contacts().getCount();
         Assertions.assertEquals(contactCount + 1, newContactCount);
     }
--- addressbook_web_tests/src/test/java/tests/GroupCreationTest.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTest.java
@@ -2,23 +2,40 @@ package tests;
 
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class GroupCreationTest extends TestBase {
 
-    public static List<String> groupNameProvider() {
-        var result = new ArrayList<String>(List.of("group name", "group name'"));
-        for (int i = 0; i < 5; i++){
-            result.add(randomString(i * 10));
+//    public static List<String> groupNameProvider() {
+//        var result = new ArrayList<String>(List.of("group name", "group name'"));
+//        for (int i = 0; i < 5; i++){
+//            result.add(randomString(i * 10));
+//        }
+//        return result;
+//    }
+
+    public static List<GroupData> groupProvider() {
+        var result = new ArrayList<GroupData>();
+        for (var name : List.of("", "group name")) {
+            for (var header : List.of("", "group header")) {
+                for (var footer : List.of("", "group footer")) {
+                    result.add(new GroupData(name, header, footer));
+                }
+            }
         }
+        for (int i = 0; i < 5; i++){
+            result.add(new GroupData(randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+        }
+        return result;
+    }
+
+    public static List<GroupData> negativeGroupProvider() {
+        var result = new ArrayList<GroupData>(List.of(
+                new GroupData("group name'", "", "")));
         return result;
     }
 
@@ -50,16 +67,16 @@ public class GroupCreationTest extends TestBase {
 //    }
 
 
-    @Test
-    public void CanCreateGroupWithEmptyName() {
-        app.groups().createGroup(new GroupData());
-    }
+//    @Test
+//    public void CanCreateGroupWithEmptyName() {
+//        app.groups().createGroup(new GroupData());
+//    }
 
 
-    @Test
-    public void CanCreateGroupWithNameOnly() {
-        app.groups().createGroup(new GroupData().withName("some name"));
-    }
+//    @Test
+//    public void CanCreateGroupWithNameOnly() {
+//        app.groups().createGroup(new GroupData().withName("some name"));
+//    }
 
 //    @Test
 //    public void CanCreateMultipleGroups() {
@@ -74,17 +91,35 @@ public class GroupCreationTest extends TestBase {
 //        Assertions.assertEquals(groupCount + n, newGroupCount);
 //    }
 
+//    @ParameterizedTest
+//    @MethodSource("groupNameProvider")
+//    public void CanCreateMultipleGroups(String name) {
+//
+//        int groupCount = app.groups().getCount();
+//
+//
+//            app.groups().createGroup(new GroupData(name, "GR header", "GR footer"));
+//
+//
+//        int newGroupCount = app.groups().getCount();
+//        Assertions.assertEquals(groupCount + 1, newGroupCount);
+//    }
+
     @ParameterizedTest
-    @MethodSource("groupNameProvider")
-    public void CanCreateMultipleGroups(String name) {
-
+    @MethodSource("groupProvider")
+    public void CanCreateMultipleGroups(GroupData group) {
         int groupCount = app.groups().getCount();
-
-
-            app.groups().createGroup(new GroupData(name, "GR header", "GR footer"));
-
-
+        app.groups().createGroup(group);
         int newGroupCount = app.groups().getCount();
         Assertions.assertEquals(groupCount + 1, newGroupCount);
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativeGroupProvider")
+    public void CanNotCreateGroups(GroupData group) {
+        int groupCount = app.groups().getCount();
+        app.groups().createGroup(group);
+        int newGroupCount = app.groups().getCount();
+        Assertions.assertEquals(groupCount, newGroupCount);
     }
 }
