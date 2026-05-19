ea71dc68be63eaf1ee2cb72cee43f3878d3a5d09
arxsnb <arxsnb@mail.ru>
2026-05-19 17:38:16 +0300

    Лекция 5,4

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -4,7 +4,11 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import common.CommonFunctions;
 import model.GroupData;
+import com.fasterxml.jackson.databind.ObjectMapper;        // ← правильный импорт
+import com.fasterxml.jackson.databind.SerializationFeature; // ← правильный импорт
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -23,7 +27,7 @@ public class Generator {
     int count;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var generator = new Generator();
         JCommander.newBuilder()
                 .addObject(generator)
@@ -33,12 +37,19 @@ public class Generator {
         generator.run();
     }
 
-    private void run() {
+    private void run() throws IOException {
         var data = generate();
         save(data);
     }
 
-    private void save(Object data) {
+    private void save(Object data) throws IOException {
+        if ("json".equals(format)) {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            mapper.writeValue(new File(output), data);
+        } else {
+            throw new IllegalArgumentException("Неизвестный формат данных " + format);
+        }
 
     }
 
